db8da201662198daa553825b865557c05f3d00cd
Kyler Li <loukylor@gmail.com>
2024-11-12 02:05:53 -0800

    setup build, add intro, use git lfs

--- Assets/Scripts/Managers/ApplicationManager.cs
+++ Assets/Scripts/Managers/ApplicationManager.cs
@@ -1,4 +1,8 @@
-﻿namespace HGDFall2024.Managers
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace HGDFall2024.Managers
 {
     public class ApplicationManager : BaseManager
     {
@@ -6,9 +10,64 @@
 
         public bool HasQuit { get; private set; } = false;
 
+        protected override void Awake()
+        {
+            base.Awake();
+
+            SceneManager.activeSceneChanged += OnSceneChange;
+
+            if (!Application.isEditor || SceneManager.GetActiveScene().buildIndex == 0)
+            {
+                LoadIntro();
+            }
+        }
+
+        private void OnSceneChange(Scene _, Scene scene)
+        {
+            switch (scene.buildIndex)
+            {
+                case 0:
+                    break;
+                case 1:
+                    Button skipButton = GameObject.Find("Canvas").transform
+                        .Find("Background/Skip").GetComponent<Button>();
+
+                    skipButton.onClick.AddListener(LoadMainMenu);
+                    break;
+                case 2:
+                    break;
+                default:
+                    break;
+            }
+        }
+
         private void OnApplicationQuit()
         {
             HasQuit = true;
+        }
+
+        public void LoadIntro()
+        {
+            SceneManager.LoadScene(1, LoadSceneMode.Single);
+        }
+
+        public void LoadMainMenu()
+        {
+            SceneManager.LoadScene(2, LoadSceneMode.Single);
+        }
+
+        public void LoadLevel(int level)
+        {
+            SceneManager.LoadScene(3 + level - 1, LoadSceneMode.Single);
+        }
+
+        public void FinishLevel(int level)
+        {
+            if (ProgressManager.Instance.AvailableLevels < level)
+            {
+                ProgressManager.Instance.AvailableLevels = (uint)level;
+            }
+            LoadLevel(level);
         }
     }
 }
