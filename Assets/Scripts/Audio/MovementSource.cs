daff049d7bae431dcea655a3198dc1a55d333962
Kyler Li <loukylor@gmail.com>
2024-11-24 15:27:14 -0800

    add sounds to pickup

---
+++ Assets/Scripts/Audio/MovementSource.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+namespace HGDFall2024.Audio
+{
+    [RequireComponent(typeof(RandomAudioSource))]
+    public class MovementSource : MonoBehaviour
+    {
+        public new Rigidbody2D rigidbody;
+        public float scale = 1;
+
+        private RandomAudioSource source;
+
+        private void Start()
+        {
+            source = GetComponent<RandomAudioSource>();
+        }
+
+        private void FixedUpdate()
+        {
+            if (source == null)
+            {
+                return;
+            }
+
+            if (rigidbody.velocity.magnitude < 0.5f)
+            {
+                source.Source.Stop();
+            }
+            else if (!source.Source.isPlaying)
+            {
+                source.Play();
+            }
+
+            source.Source.volume = rigidbody.velocity.magnitude * 0.1f * scale;
+        }
+    }
+}
