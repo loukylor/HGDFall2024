e7bdb2dc11f82b1435034bb4af97b459a1d05ca1
Kyler Li <loukylor@gmail.com>
2024-11-24 18:50:47 -0800

    add audio to other levels

--- Assets/Scripts/Audio/CollisionSource.cs
+++ Assets/Scripts/Audio/CollisionSource.cs
@@ -6,12 +6,23 @@ namespace HGDFall2024.Audio
     [RequireComponent(typeof(Collider2D))]
     public class CollisionSource : MonoBehaviour
     {
+        public bool scaleSpeed = true;
+
         private void OnCollisionEnter2D(Collision2D collision)
         {
             float scale = collision.relativeVelocity.magnitude * 0.05f;
 
             RandomAudioSource source = GetComponent<RandomAudioSource>();
-            source.Source.volume = scale;
+            if (scaleSpeed)
+            {
+                source.Source.volume = scale;
+            }
+            source.Play();
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            RandomAudioSource source = GetComponent<RandomAudioSource>();
             source.Play();
         }
     }
